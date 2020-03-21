8b1561489e4a48d9c09a124e84eabb5ed546e662
Skyfall <thefallk@mail.ru>
2020-03-21 23:59:28 +0500

    add options to chose players count and players type

--- loto_utils.py
+++ loto_utils.py
@@ -3,6 +3,28 @@ import random
 MAX_NUMBERS = 90
 NUMBERS_IN_CARS = 15
 
+
+def generate_random_seq(max_, count, min_=1, sort_flag=False):
+    """
+    Генерирует count случайных чисел в
+    промежутке от min_ до max_ без повторений
+    :param max_: верхний порог чисел
+    :param min_: ничжний порог чисел
+    :param count: количесвто чисел в последовательности
+    :return: список случайных count чисел от min_ до max_ без повторений
+    """
+
+    assert max_ - min_ > count, 'Слишком много чисел для промежутка!'
+
+    list_ = list(range(min_, max_ + 1))
+    random.shuffle(list_)
+    res = list_[:count]
+    if sort_flag:
+        res.sort()
+
+    return res
+
+
 class Bag:
     """
     Класс 'Мешок' для хранения бочонков
@@ -38,62 +60,185 @@ class Bag:
         self.__del_number(number)
         return number
 
+
 class Card:
 
-    def __init__(self, max_number, number_in_card = NUMBERS_IN_CARS):
-        numbers = list(range(1, max_number))
-        random.shuffle(numbers)
-        self.numbers = numbers[:number_in_card]
+    def __init__(self,
+                 max_number,
+                 number_in_card=NUMBERS_IN_CARS,
+                 lines_count=3,
+                 num_in_line=9):
+        self.num_in_line = num_in_line
+        self.numbers = generate_random_seq(max_number, number_in_card)
+        step = NUMBERS_IN_CARS // lines_count
+        self.card_lines = {'line_' + str(i + 1): self.numbers[i * step:(i + 1) * step] for i in range(lines_count)}
+
+        for key in self.card_lines.keys():
+            numbers = self.card_lines[key]
+            numbers.sort()
+            self.card_lines[key] = numbers
+
+        self.lines_position = {
+            'line_' + str(i + 1): generate_random_seq(self.num_in_line - 1, 5, min_=0, sort_flag=True) for i in
+            range(lines_count)}
         self.numbers.sort()
         self.num_in_card = len(self.numbers)
+        self.length_card = 3 * self.num_in_line
 
     def cross_out(self, number):
-        if number in self.numbers:
-            idx = self.numbers.index(number)
-            self.numbers[idx] = '-'
-            self.num_in_card -= 1
-        else:
-            return -1
+        for nums in self.card_lines.values():
+            if number in nums:
+                idx = nums.index(number)
+                nums[idx] = '-'
+                self.num_in_card -= 1
+                res = None
+                break
+            else:
+                res = -1
+        return res
+
+    def prepear_card(self):
+
+        res_str = ''
+        for line in self.card_lines.keys():
+            nums = self.card_lines[line]
+            possition = self.lines_position[line]
+            for i in range(self.num_in_line):
+                if i in possition:
+                    idx = possition.index(i)
+                    num = nums[idx]
+                    if num != '-':
+                        if num < 10:
+                            res_str += ' ' + str(num) + ' '
+                        else:
+                            res_str += str(num) + ' '
+                    else:
+                        res_str += ' ' + num + ' '
+                else:
+                    res_str += '   '
+            res_str += '\n'
+
+        return res_str
 
 
 class Player:
+    players_type = ('Человек', 'Компьютер')
+    human_players = 0
 
-    def __init__(self, name):
+    def __init__(self, name, player_type):
         self.name = name
         self.card = Card(MAX_NUMBERS)
+        self.player_type = player_type
+        if self.player_type == 'Человек':
+            Player.human_players += 1
+
+    @staticmethod
+    def humans_in_game():
+        return Player.human_players
 
     def cross_out(self, number):
         return self.card.cross_out(number)
 
+    def show_card(self):
+        # print(self.card.length_card)
+        start_count = (self.card.length_card - len(self.name)) // 2
+        header = start_count * '-' + self.name + start_count * '-'
+        end = '-' * self.card.length_card + '\n'
+        res = '\n'.join([header,
+                         self.card.prepear_card()])
+        res += end
 
-bag = Bag(MAX_NUMBERS)
-player_0 = Player('Игрок')
-player_1 = Player('Компьютер')
-while True:
-    current_num = bag.chouse_number()
-    print('Выбран номер:', current_num, '(Осталось {})'.format(bag.check_number()))
-    print('Карточка', player_0.name)
-    print(player_0.card.numbers)
-    print('Карточка', player_1.name)
-    print(player_1.card.numbers)
-    if current_num in player_1.card.numbers:
-        player_1.cross_out(current_num)
-    answer = input('Зачеркнуть число? (y,n)')
-    if answer == 'y':
-        if player_0.cross_out(current_num) == -1:
-            print('Вы проиграли!')
-            exit()
-    elif answer == 'n':
-        if player_0.cross_out(current_num) == -1:
-            pass
-        else:
-            print('Вы проиграли!')
-            exit()
+        return res
 
-    if player_0.card.num_in_card == 0:
-        print('Победил игрок ', player_0.name)
-        exit()
+def main():
+    bag = Bag(MAX_NUMBERS)
 
-    if player_1.card.num_in_card == 0:
-        print('Победил игрок ', player_1.name)
-        exit()
+    while True:
+        players_count = input('Введите количство игроков от 2 до 10')
+
+        try:
+            players_count = int(players_count)
+            if players_count < 2 or players_count > 10:
+                print('Игроков не может быть меньше 2 или больше 10')
+            else:
+                break
+        except:
+            print('Введите число от 2 до 10')
+
+    players = []
+    players_type = {str(i + 1): Player.players_type[i] for i in range(len(Player.players_type))}
+
+    for i in range(players_count):
+        # print('Игрок номер', i+1, '\n' + 'Выбеите тип игрока:')
+
+        while True:
+            print('Игрок номер', i + 1, '\n' + 'Выбеите тип игрока:')
+            for key in players_type.keys():
+                print(key, players_type[key])
+
+            input_player_type = input()
+
+            if input_player_type in players_type.keys():
+                player_type = players_type[input_player_type]
+                break
+            else:
+                print('Неверный выбор!')
+
+        player_name = input('Введите имя игрока: \n')
+
+        players.append(Player(player_name, player_type))
+
+    humans_in_game = Player.humans_in_game()
+    losse_players = []
+    while True:
+
+        while losse_players:
+            l_pl = losse_players.pop()
+            players.remove(l_pl)
+
+        current_num = bag.chouse_number()
+        print('Выбран номер:', current_num, '(Осталось {})'.format(bag.check_number()))
+
+        for n, player in enumerate(players):
+
+            print(player.show_card())
+
+            if player.player_type == 'Компьютер':
+
+                if current_num in player.card.numbers:
+                    player.cross_out(current_num)
+
+            elif player.player_type == 'Человек':
+                answer = input('Зачеркнуть число? (y,n)')
+
+                if answer == 'y':
+
+                    if player.cross_out(current_num) == -1:
+                        print('Игрок {} проиграл!'.format(player.name))
+                        losse_players.append(player)
+                        humans_in_game -= 1
+
+                        if humans_in_game == 0:
+                            print("Все люди проиграли!")
+                            exit()
+
+                elif answer == 'n':
+
+                    if player.cross_out(current_num) == -1:
+                        pass
+
+                    else:
+                        print('Игрок {} проиграл!'.format(player.name))
+                        losse_players.append(player)
+                        humans_in_game -= 1
+
+                        if humans_in_game == 0:
+                            print("Все люди проиграли!")
+                            exit()
+
+                if player.card.num_in_card == 0:
+                    print('Игрок {} победил!'.format(player.name))
+                    exit()
+
+
+main()
